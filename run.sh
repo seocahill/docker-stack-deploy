807e05004bf30fb23d520e6a00dd673c8a412d67
Seosamh Cahill <seo.cahill@gmail.com>
2018-03-29 15:40:43 +0200

    Set authentication secret in startup script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/ash
 
 set -e
+export AUTHENTICATION_SECRET=$(cat /run/secrets/basic_auth_password)
 cat /run/secrets/dockerhub_password | docker login --username $DOCKER_USERNAME --password-stdin
 ruby app.rb

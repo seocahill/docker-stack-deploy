413bbe0565e2275d957dd97c51266c68578c3702
Seosamh Cahill <seo.cahill@gmail.com>
2018-07-16 17:54:22 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/ash
 
 set -e
-export AUTHENTICATION_SECRET=$(cat /run/secrets/basic_auth_password)
-cat /run/secrets/dockerhub_password | docker login --username $DOCKER_USERNAME --password-stdin
+mkdir /src
+git clone $APP_REPO /src
 ruby app.rb

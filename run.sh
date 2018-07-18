b6107ccd245608a67b6cf53dd05d3303eb229d12
Seosamh Cahill <seo.cahill@gmail.com>
2018-07-18 10:29:10 +0200

    Update start script to pull stack repo

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #! /bin/ash
 
 set -e
-git clone $(cat /run/secrets/github_uri) /src 
+
+REPOSRC=$( cat /run/secrets/github_uri )
+LOCALREPO=/src
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull $REPOSRC
+fi
+
+cd /app
+
 ruby app.rb

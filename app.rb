f296c409d30593073abc7504228ebd6184f791c3
Seosamh Cahill <seo.cahill@gmail.com>
2018-07-18 17:51:27 +0200

    Various update (#1)
    Various updates to deploy and info endpoints

--- app.rb
+++ app.rb
@@ -1,66 +1,111 @@
 require 'sinatra'
 require 'slack-notifier'
+require 'open3'
 
 set :logging, true
 set :run, true
 set :bind, '0.0.0.0'
 
-use Rack::Auth::Basic, "Authentication failed" do |username, password|
-  username == 'deploy' and password == ENV.fetch('AUTHENTICATION_SECRET', 'deploy')
+before do
+  return if params['token'] == %x{ cat /run/secrets/auth_token }.strip
+  halt 401, "Not authorized\n"
 end
 
-get '/' do
-  message = %x{docker service ls}
-  notify(message)
-  200
+post '/info' do
+  service = params['text']
+  channel = "##{params['channel_name']}"
+  if service.nil? || service.empty?
+    stdout, stderr, status = Open3.capture3('docker service ls')
+  else
+    stdout, stderr, status = Open3.capture3("docker service ps #{service}")
+  end
+  status.success? ? notify("```#{stdout}```", channel) : notify(stderr, channel)
+  status
 end
 
-get '/redeploy' do
-  notify(redeploy)
-  200
+post '/deploy' do
+  if login
+    Thread.new { deploy }
+    [200, 'Deploying in the background, might take a while...']
+  else
+    [403, 'Error! docker login failed']
+  end
 end
 
-get '/update/:service_name' do
-  message = update(params['service_name'])
-  notify(message)
-  200
+post '/update' do
+  if login
+    Thread.new { update(params) }
+    [200, 'Updating service in the background, might take a while...']
+  else
+    [403, 'Error! docker login failed']
+  end
 end
 
 private
 
-def redeploy
-  system "docker stack deploy -c /run/secrets/docker-stack.yml"
+def deploy
+  # Change to directory of cloned stack code
+  Dir.chdir('/src')
 
-  if $?.exitstatus == 0
-    "Stack was redeployed"
+  # Update config to latest and redeploy
+  remote = %x{ cat /run/secrets/github_uri }
+  command = %Q{
+    git checkout master &&
+    git fetch #{remote} &&
+    git reset --hard origin/master &&
+    docker stack deploy -c $STACK_CONFIG_FILE $STACK_NAME --with-registry-auth
+  }
+
+  stdout, stderr, status = Open3.capture3(command)
+
+  if status.success?
+    notify("```Success! #{stdout}```")
   else
-    "Stack could not be re-deployed"
+    notify("```Error! #{stderr}```")
   end
 end
 
 def login 
-  system "docker login"
+  system "cat /run/secrets/dockerhub_password | docker login --username $DOCKER_USERNAME --password-stdin"
   $?.exitstatus == 0
 end
 
-def update(service)
-  raise unless ENV['ALLOWED_SERVICES'].split(',').include?(service)
+def update(params)
+  service, tag = params.dig('text').split(':')
 
-  system "docker service update --force #{service}"
+  # Only update whitelisted services
+  whitelisted = ENV['ALLOWED_SERVICES'].split(',').include?(service)
+  return notify("```Error! #{service} service is not whitelisted```") unless whitelisted
 
-  if $?.exitstatus == 0
-    "Updated #{service} successfully"
+  # Apply passed in or default tag
+  image_name, image_tag = current_image(service).split(':')
+  new_tag = tag || ENV.fetch('DEFAULT_TAG', 'dev')
+
+  command = "docker service update --image #{image_name}:#{new_tag} --force #{service} --with-registry-auth"
+  stdout, stderr, status = Open3.capture3(command)
+
+  if status.success?
+    new_image = current_image(service)
+    notify("```Success! Image was updated to #{new_image}```")
   else
-    "Updated #{service} failed"
+    notify("```Error! #{stderr}```")
   end
+end
+
+def current_image(service)
+  %x{docker service inspect #{service} -f {{.Spec.TaskTemplate.ContainerSpec.Image}}}.split('@').first
 end
 
 def slack
   @slack ||= Slack::Notifier.new(ENV["WEBHOOK_URL"])
 end
 
-def notify(message)
+def notify(message, channel = nil)
   logger.info(message)
-  slack.ping(message)
+  begin
+    slack.ping(message, channel: channel)
+  rescue
+    slack.ping(message)
+  end
 end
 
--- run.sh
+++ run.sh
@@ -1,6 +1,21 @@
 #! /bin/ash
 
 set -e
-export AUTHENTICATION_SECRET=$(cat /run/secrets/basic_auth_password)
-cat /run/secrets/dockerhub_password | docker login --username $DOCKER_USERNAME --password-stdin
+
+REPOSRC=$( cat /run/secrets/github_uri )
+LOCALREPO=/src
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull $REPOSRC
+fi
+
+cd /app
+
 ruby app.rb
